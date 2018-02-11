6fa452ac4ada55e4c29941ea39c2199ea5d9e7a1
Dave Richer <dave@ir0ny.com>
2018-02-11 03:23:25 -0500

    removed old char test, which was kinda a fake test to begin with

--- test/test-convert-encoding.js
+++ test/test-convert-encoding.js
@@ -10,35 +10,6 @@ var bindTo = {
     }
 };
 
-test('irc.Client.convertEncoding old', function(assert) {
-    var convertEncoding = function(str) {
-        var self = this;
-
-        if (self.opt.encoding) {
-            var charsetDetector = require('node-icu-charset-detector');
-            var Iconv = require('iconv').Iconv;
-            var charset = charsetDetector.detectCharset(str).toString();
-            var to = new Iconv(charset, self.opt.encoding);
-
-            return to.convert(str);
-        }
-        return str;
-    }.bind(bindTo);
-
-    checks.causesException.forEach(function iterate(line) {
-        var causedException = false;
-        try {
-            convertEncoding(line);
-        } catch (e) {
-            causedException = true;
-        }
-
-        assert.equal(causedException, true, line + ' caused exception');
-    });
-
-    assert.end();
-});
-
 test('irc.Client.convertEncoding', function(assert) {
     var convertEncoding = irc.Client.prototype.convertEncoding.bind(bindTo);
 
